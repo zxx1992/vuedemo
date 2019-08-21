b3be5873239d901ca717d1a91dc705eb79583400
zhengxiaoxiao <18255068953@163.com>
2019-08-21 18:24:03 +0800

    feat: chapter6.1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,28 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" :class="{'default': true,'set':true}"/>
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <Input type="text"/>
+    <!-- v-model指令的双向数据绑定 -->
+    <Input type="text" v-model="message"/>
+    <!-- <Input type="text" @input="handleInput"/> -->
+    <p  style="white-space: nowrap">输入的内容：{{message}}</p>
+    <div class="radio">
+    <input type="radio" v-model="picked" value="html" id="html">
+    <label for="html">html</label>
+    <input type="radio" v-model="picked" value="js" id="js">
+    <label for="js">js</label>
+    <input type="radio" v-model="picked" value="css" id="css">
+    <label for="css">css</label>
+    <p>选中的项：{{picked}}</p>
+    </div>
+    <div class="checkbox">
+    <input type="checkbox" v-model="checked" value="html" id="html">
+    <label for="html">html</label>
+    <input type="checkbox" v-model="checked" value="js" id="js">
+    <label for="js">js</label>
+    <input type="checkbox" v-model="checked" value="css" id="css">
+    <label for="css">css</label>
+    <p>选中的项：{{checked}}</p>
+    </div>
   </div>
 </template>
 
@@ -21,7 +42,21 @@ export default {
   },
   data() {
     return {
-
+      message: "",
+      picked: "js",
+      checked: ['html','js']
+    }
+  },
+  methods: {
+    // handleInput: function(e) {
+    //   console.log(e)
+    //   this.message = e;
+    // }
+  },
+  watch: {
+    message(newVal,old) {
+      console.log(newVal)
+      console.log(old)
     }
   }
 };
